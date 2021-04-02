b40603b8f7bb6a8a02fac11b50b8b30c52a4ba04
Satoshi-Kusumoto <29630164+Satoshi-Kusumoto@users.noreply.github.com>
2021-04-02 17:54:37 +0800

    bump runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -69,10 +69,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("litentry-parachain"),
 	impl_name: create_runtime_str!("litentry-parachain"),
 	authoring_version: 1,
-	spec_version: 3,
-	impl_version: 1,
+	spec_version: 4,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 };
 
 pub const MILLISECS_PER_BLOCK: u64 = 6000;
